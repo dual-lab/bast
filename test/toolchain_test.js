dca5de586d07df1eaf1a55c5b18b67dcc328f5e3
dmike16 <cipmiky@gmail.com>
2020-03-15 23:42:03 +0100

    test: 1/4 add test to check correct options

--- test/toolchain_test.js
+++ test/toolchain_test.js
@@ -1,6 +1,44 @@
 const test = require('ava');
+const proxyquire = require('proxyquire');
 
-test.todo('Compiler hook is register with correct options.');
+let registedCompiler = null;
+let registedOpts = null;
+
+test.before('Proxy modules pirates and source-map-support', t => {
+	t.context = proxyquire('../dist/bast', {
+		'pirates': {
+			addHook: (currentCompiler, opts) => {
+				registedCompiler = currentCompiler;
+				registedOpts = opts;
+
+				return () => {
+					registedCompiler = null;
+					registedOpts = null;
+				};
+			}
+		},
+		'source-map-support': {
+			install() {
+				installedSourceMap = true;
+			}
+		}
+	});
+});
+
+let installedSourceMap = false;
+
+
+test('Compiler hook is register with correct options.', t => {
+	t.plan(2);
+
+	const uninstall = t.context.install({ extensions: ['.ts'] });
+
+	t.assert(registedOpts.exts.length > 0, 'Extensions are registered');
+	t.is(registedOpts.exts[0], '.ts');
+
+	uninstall();
+});
+
 test.todo('Cache is correctly loaded');
 test.todo('Source map is installed.');
 test.todo('Source code is transfomed with babel compiler');
