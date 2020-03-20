377c2bb0de5e487e6aeb87f9c789e2b63f8fc441
dmike16 <cipmiky@gmail.com>
2020-03-20 23:39:33 +0100

    test: add jit compiler test case

--- test/toolchain_test.js
+++ test/toolchain_test.js
@@ -1,12 +1,16 @@
 const test = require('ava');
 const proxyquire = require('proxyquire');
+const fs = require('fs');
+const path = require('path');
+const fixtures_dir = path.resolve(__dirname, './__fixture__');
 
 let registedCompiler = null;
 let registedOpts = null;
+let installedSourceMap = false;
 
 test.before('Proxy modules pirates and source-map-support', t => {
 	t.context = proxyquire('../dist/bast', {
-		'pirates': {
+		pirates: {
 			addHook: (currentCompiler, opts) => {
 				registedCompiler = currentCompiler;
 				registedOpts = opts;
@@ -25,9 +29,6 @@ test.before('Proxy modules pirates and source-map-support', t => {
 	});
 });
 
-let installedSourceMap = false;
-
-
 test('Compiler hook is register with correct options.', t => {
 	t.plan(2);
 
@@ -40,5 +41,46 @@ test('Compiler hook is register with correct options.', t => {
 });
 
 test.todo('Cache is correctly loaded');
-test.todo('Source map is installed.');
-test.todo('Source code is transfomed with babel compiler');
+
+test('Source map is installed.', t => {
+	t.plan(1);
+
+	const uninstall = t.context.install({
+		extensions: ['.ts'],
+		sourceMaps: true
+	});
+	const code = fs.readFileSync(
+		path.join(fixtures_dir, 'tobe-parsed.js'),
+		'utf8'
+	);
+	registedCompiler(code, 'tobe-parsed.js');
+
+	t.true(installedSourceMap, 'Source map is installed');
+	uninstall();
+});
+
+test('Source code is transfomed with babel compiler', t => {
+	t.plan(2);
+
+	const uninstall = t.context.install({
+		extensions: ['.ts'],
+		sourceMaps: false,
+		babelrc: false,
+		presets: ['@babel/preset-env']
+	});
+	const code = fs.readFileSync(
+		path.join(fixtures_dir, 'tobe-parsed.js'),
+		'utf8'
+	);
+	const babelCode = registedCompiler(code, 'single-import');
+
+	t.false(!installedSourceMap, 'Source map is installed');
+	t.is(
+		babelCode,
+		`"use strict";
+
+var test = true;`,
+		'Code is correctly parsed'
+	);
+	uninstall();
+});
