827c4f7f4b757bffeb368fe15c70f28efd4858d4
dmike <cipmiky@gmail.com>
2020-09-13 17:56:00 +0200

    build: patch version 1.0.1 with audit fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import json from '@rollup/plugin-json';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 
 import { main, module, dependencies, peerDependencies } from './package.json';
 
